d0b71786a9e7500a0213ec3bf787e513cb562ea7
DK-AC <Denkacaj@gmail.com>
2023-10-20 11:43:20 +0300

    added all variants story typography

--- src/components/typography/typography.stories.tsx
+++ src/components/typography/typography.stories.tsx
@@ -10,11 +10,54 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+export const AllVariants: () => JSX.Element = () => (
+  <>
+    <Typography as="span" variant="large">
+      Large
+    </Typography>
+    <Typography as="h1" variant="heading1">
+      Heading1
+    </Typography>
+    <Typography as="h2" variant="heading2">
+      Heading2
+    </Typography>
+    <Typography as="h3" variant="heading3">
+      Heading3
+    </Typography>
+    <Typography as="p" variant="body1">
+      Body1
+    </Typography>
+    <Typography as="p" variant="body2">
+      Body2
+    </Typography>
+    <Typography as="p" variant="subtitle1">
+      Subtitle1
+    </Typography>
+    <Typography as="p" variant="subtitle2">
+      Subtitle2
+    </Typography>
+    <Typography as="span" variant="caption">
+      Caption
+    </Typography>
+    <Typography as="p" variant="overline">
+      Overline
+    </Typography>
+    <Typography as="a" variant="link1">
+      Link1
+    </Typography>
+    <div>
+      <Typography as="a" variant="link2">
+        Link2
+      </Typography>
+    </div>
+  </>
+)
+
 export const Large: Story = {
   args: {
     as: 'span',
     variant: 'large',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Large',
   },
 }
 
@@ -22,7 +65,7 @@ export const Heading1: Story = {
   args: {
     as: 'h1',
     variant: 'heading1',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Heading1',
   },
 }
 
@@ -30,7 +73,7 @@ export const Heading2: Story = {
   args: {
     as: 'h2',
     variant: 'heading2',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Heading2',
   },
 }
 
@@ -38,7 +81,7 @@ export const Heading3: Story = {
   args: {
     as: 'h3',
     variant: 'heading3',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Heading3',
   },
 }
 
@@ -46,7 +89,7 @@ export const Body1: Story = {
   args: {
     as: 'p',
     variant: 'body1',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Body1',
   },
 }
 
@@ -54,7 +97,7 @@ export const Body2: Story = {
   args: {
     as: 'p',
     variant: 'body2',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Body2',
   },
 }
 
@@ -62,7 +105,7 @@ export const Subtitle1: Story = {
   args: {
     as: 'p',
     variant: 'subtitle1',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Subtitle1',
   },
 }
 
@@ -70,7 +113,7 @@ export const Subtitle2: Story = {
   args: {
     as: 'p',
     variant: 'subtitle2',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Subtitle2',
   },
 }
 
@@ -78,7 +121,7 @@ export const Caption: Story = {
   args: {
     as: 'span',
     variant: 'caption',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Caption',
   },
 }
 
@@ -86,7 +129,7 @@ export const Overline: Story = {
   args: {
     as: 'p',
     variant: 'overline',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Overline',
   },
 }
 
@@ -94,7 +137,7 @@ export const Link1: Story = {
   args: {
     as: 'a',
     variant: 'link1',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Link1',
   },
 }
 
@@ -102,6 +145,6 @@ export const Link2: Story = {
   args: {
     as: 'a',
     variant: 'link2',
-    children: `Carosserie Test Zürich Stauffacherstrasse 31 8004 Zürich, ZH, CH`,
+    children: 'Link2',
   },
 }
