a83c3ee008cb8970d963c833de4656eaf3a9cb92
DK-AC <Denkacaj@gmail.com>
2023-10-25 14:03:05 +0300

    added createNewPassword

---
+++ src/components/auth/createNewPassword/createNewPassword.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { CreateNewPassword } from './createNewPassword'
+
+const meta = {
+  component: CreateNewPassword,
+  parameters: {
+    backgrounds: {
+      default: 'black',
+      values: [
+        { name: 'white', value: '#fff' },
+        { name: 'black', value: '#000' },
+      ],
+    },
+  },
+  tags: ['autodocs'],
+  title: 'Components/auth/createNewPassword',
+} satisfies Meta<typeof CreateNewPassword>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CreateNewPasswordDemo: Story = {
+  args: {
+    children: <CreateNewPassword />,
+  },
+  render: () => <CreateNewPassword />,
+}
