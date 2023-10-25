3ce83d8efee40d2654879e6b3b01879998f91d0a
DK-AC <Denkacaj@gmail.com>
2023-10-25 13:43:56 +0300

    added checkEmail

---
+++ src/components/auth/checkEmail/checkEmail.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { CheckEmail } from './checkEmail'
+
+const meta = {
+  component: CheckEmail,
+  parameters: {
+    backgrounds: {
+      default: 'black',
+      values: [
+        { name: 'white', value: '#fff' },
+        { name: 'black', value: '#000' },
+      ],
+    },
+  },
+  tags: ['autodocs'],
+  title: 'Components/auth/checkEmail',
+} satisfies Meta<typeof CheckEmail>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CheckEmailDemo: Story = {
+  args: {
+    children: <CheckEmail />,
+  },
+  render: () => <CheckEmail />,
+}
