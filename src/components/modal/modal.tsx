51c6e76eb9cc1109f6501a215588dc32950458a9
DK-AC <Denkacaj@gmail.com>
2023-10-26 10:53:57 +0300

    forward ref to modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,29 +1,32 @@
-import { FC, ReactNode } from 'react'
-import * as Dialog from '@radix-ui/react-dialog'
+import { ComponentPropsWithoutRef, FC, forwardRef, ReactNode } from 'react'
+import * as DialogRadix from '@radix-ui/react-dialog'
 import { Typography } from '@/components/typography'
 import { Close } from '@/asserts/icons/components/Close'
 import s from './modal.module.scss'
 import { Button } from '@/components/button'
 
-type Props = { children?: ReactNode }
-
-export const Modal: FC<Props> = ({ children }) => {
-  return (
-    <div className={s.modal}>
-      <Dialog.Root>
-        <Dialog.Trigger className={s.dialogTrigger} asChild>
-          <Button variant={'primary'} type={'button'}>
-            Edit
-          </Button>
-        </Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay className={s.dialogOverlay} />
-          <Dialog.Content className={s.dialogContent}>{children}</Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-    </div>
-  )
-}
+export const Modal = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<typeof DialogRadix.Root>>(
+  (props, ref) => {
+    const { modal, children, open, onOpenChange, ...rest } = props
+    return (
+      <div className={s.modal}>
+        <DialogRadix.Root open={open} onOpenChange={onOpenChange}>
+          <DialogRadix.Trigger className={s.dialogTrigger} asChild>
+            <Button variant={'primary'} type={'button'}>
+              Edit
+            </Button>
+          </DialogRadix.Trigger>
+          <DialogRadix.Portal>
+            <DialogRadix.Overlay className={s.dialogOverlay} />
+            <DialogRadix.Content className={s.dialogContent} {...rest} ref={ref}>
+              {children}
+            </DialogRadix.Content>
+          </DialogRadix.Portal>
+        </DialogRadix.Root>
+      </div>
+    )
+  }
+)
 
 type ModalTitleProps = {
   title?: string
@@ -31,22 +34,22 @@ type ModalTitleProps = {
 
 export const ModalTitle: FC<ModalTitleProps> = ({ title }) => {
   return (
-    <Dialog.Title className={s.dialogTitle}>
+    <DialogRadix.Title className={s.dialogTitle}>
       <Typography variant={'heading2'} as={'h2'}>
         {title}
       </Typography>
       <ModalClose />
-    </Dialog.Title>
+    </DialogRadix.Title>
   )
 }
 
 export const ModalClose = () => {
   return (
-    <Dialog.Close>
+    <DialogRadix.Close aria-label="Close">
       <button className={s.dialogClose}>
         <Close size={24} />
       </button>
-    </Dialog.Close>
+    </DialogRadix.Close>
   )
 }
 
@@ -55,5 +58,5 @@ type ModalDescriptionProps = {
 }
 
 export const ModalDescription: FC<ModalDescriptionProps> = ({ children }) => {
-  return <Dialog.Description className={s.description}>{children}</Dialog.Description>
+  return <DialogRadix.Description className={s.description}>{children}</DialogRadix.Description>
 }
