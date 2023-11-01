837306d12b9c2fcc68b38b56f9478cbad1fd6a1c
DK-AC <Denkacaj@gmail.com>
2023-11-01 20:48:17 +0300

    delete styles ui_kit

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 
-import '@it-incubator/ui-kit/dist/style.css'
-
 import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
