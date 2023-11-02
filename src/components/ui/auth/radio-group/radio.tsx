db57a0f4372606e9205a38879ffc85cf71b1817a
nikizak34 <nikita2000.16.11@gmail.com>
2023-11-02 23:37:25 +0300

    fix radioGroup

--- src/components/ui/auth/radio-group/radio.tsx
+++ src/components/ui/auth/radio-group/radio.tsx
@@ -1,33 +1,52 @@
 import * as RadioGroup from '@radix-ui/react-radio-group';
 import s from './radio.module.scss'
-import {ComponentPropsWithoutRef} from "react";
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react";
 import * as RadioGroupLib from "@radix-ui/react-radio-group";
 import {Label} from "@/components/ui/label";
+import {Typography} from "@/components/ui/typography";
 
 type Option = {
-    value: string;
+    value: string | number;
     key?: number | string;
 } & ComponentPropsWithoutRef<"input">;
 export type RadioGroupPropsType = RadioGroupLib.RadioGroupProps & {
     options: Option[];
     disabled?: boolean;
     defaultValue?: string;
-};
-export const Radio = ({options,disabled}: RadioGroupPropsType,) => {
-    return (
-        <RadioGroup.Root className={s.RadioGroupRoot} disabled={disabled}>
-            {options?.map((el) => {
-                return (
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <RadioGroup.Item className={s.RadioGroupItem} key={el.key} value={el.value}>
-                            <RadioGroup.Indicator className={s.RadioGroupIndicator}/>
-                        </RadioGroup.Item>
-                        <Label className={s.Label}  label={'Compact'}/>
-                    </div>
-                )
-            })}
+    errorMessage?: string
 
-        </RadioGroup.Root>
-    );
 };
+export const Radio = forwardRef<
+    ElementRef<typeof RadioGroupLib.Root>,
+    RadioGroupPropsType
+>(
+    (
+        {
+            errorMessage,
+            options,
+            disabled = false,
+        }: RadioGroupPropsType,
+        ref
+    ) => {
+        return (
+            <RadioGroup.Root className={s.RadioGroupRoot} disabled={disabled} ref={ref}>
+                {options?.map((el) => {
+                    return (
+                        <div style={{display: 'flex', alignItems: 'center'}}>
+                            <RadioGroup.Item className={s.RadioGroupItem} key={el.key} value={`${el.value}`}>
+                                <RadioGroup.Indicator className={s.RadioGroupIndicator}/>
+                            </RadioGroup.Item>
+                            <Label className={s.Label} label={`${el.value}`}/>
+                        </div>
+                    )
+                })}
+                {errorMessage && (
+                    <Typography>
+                        {errorMessage}
+                    </Typography>
+                )}
+            </RadioGroup.Root>
+        );
+    }
+)
 
