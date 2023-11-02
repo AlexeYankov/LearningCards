911159a144a837a6833727466eff7b6ca1c6733d
DK-AC <Denkacaj@gmail.com>
2023-11-02 19:34:32 +0300

    signin render app and style components container

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Provider } from 'react-redux'
 
 import { store } from './api/store'
-import { Router } from './api/router'
 import { Header } from '@/components/ui/packs/components/header/header.tsx'
+import { SignIn } from '@/components/ui/auth/signIn/signIn.tsx'
+import s from './app.module.scss'
 
 export function App() {
   return (
     <Provider store={store}>
       {/* <PacksPage /> */}
-      <Header />
-
-      <Router />
+      <Header isLoggedIn={false} />
+      <div className={s.container}>
+        <SignIn />
+      </div>
+      {/*<Router />*/}
     </Provider>
   )
 }
