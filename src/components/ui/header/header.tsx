f0f6164ea2ab91f33f56f792e5b480e65e988878
DK-AC <Denkacaj@gmail.com>
2023-11-28 14:21:31 +0300

    fix: header styles

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,63 +1,64 @@
 import logo from '@/asserts/Logo.png'
-import {Button} from '@/components/ui/button'
-import {DropDownMenu} from '@/components/ui/dropDown/dropDown'
-import {Typography} from '@/components/ui/typography'
+import { Button } from '@/components/ui/button'
+import { DropDownMenu } from '@/components/ui/dropDown/dropDown'
+import { Typography } from '@/components/ui/typography'
 import s from './header.module.scss'
 import style from '../../../app.module.scss'
 import profileImage from '../../../asserts/profileImage.png'
-import {FC} from 'react'
-import {Link, Outlet} from "react-router-dom";
-import {useMeQuery} from "@/api/auth-api/auth.api.ts";
-
+import { FC } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import { useMeQuery } from '@/api/auth-api/auth.api.ts'
 
 export const Header: FC = () => {
-    const {data,isError, isLoading} = useMeQuery()
-    const isLoggedIn = !isError
-    return (
-        <div className={s.container}>
-            <header className={s.header}>
-                <Typography as={Link} to={'/'}>
-                    <img alt={''} className={s.logo} src={logo}/>
-                </Typography>
-                <div className={s.textHeader}>
-                    {!isLoading &&
-                        <>
-                            {isLoggedIn &&
-                                <DropDownMenu avatar={data?.avatar||profileImage} email={data?.email} name={data?.name}/>}
-                            {!isLoggedIn &&
-                                <Button
-                                    children={
-                                        <Typography
-                                            as={Link}
-                                            to={'/login'}
-                                            children={'Sign In'}
-                                            variant={'subtitle2'}
-                                        />}
-                                    className={s.button}
-                                    fullWidth
-                                    variant={'primary'}
-                                />}
-                        </>}
-                </div>
-            </header>
+  const { data, isError, isLoading } = useMeQuery()
+  const isLoggedIn = !isError
+  return (
+    <div className={s.container}>
+      <header className={s.header}>
+        <Typography as={Link} to={'/'}>
+          <img alt={''} className={s.logo} src={logo} />
+        </Typography>
+        <div className={s.textHeader}>
+          {!isLoading && (
+            <>
+              {isLoggedIn && (
+                <DropDownMenu
+                  avatar={data?.avatar || profileImage}
+                  email={data?.email}
+                  name={data?.name}
+                />
+              )}
+              {!isLoggedIn && (
+                <Button
+                  children={
+                    <Typography
+                      className={s.linkSignIn}
+                      as={Link}
+                      to={'/login'}
+                      children={'Sign In'}
+                      variant={'subtitle2'}
+                    />
+                  }
+                  className={s.button}
+                  fullWidth
+                  variant={'primary'}
+                />
+              )}
+            </>
+          )}
         </div>
-    )
+      </header>
+    </div>
+  )
 }
-
 
 export const Layout = () => {
-    return (
-        <>
-            <Header/>
-            <div className={style.container}>
-                <Outlet/>
-            </div>
-
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <div className={style.container}>
+        <Outlet />
+      </div>
+    </>
+  )
 }
-
-
-
-
-
