758ab8b89036b535c50705f542295eea963fb86f
DK-AC <Denkacaj@gmail.com>
2023-12-04 17:19:54 +0300

    add: learn random card

--- src/api/decks/decks.api.ts
+++ src/api/decks/decks.api.ts
@@ -1,135 +1,135 @@
-import {baseApi} from '@/api/cards.api.ts'
-import {PaginationResponseType} from '@/api/common.api.ts'
-import {Sort} from '@/components/ui/table/types.ts'
+import { baseApi } from '@/api/cards.api.ts'
+import { CardsResponseType, PaginationResponseType } from '@/api/common.api.ts'
+import { Sort } from '@/components/ui/table/types.ts'
 
 type GetDecksParamsType = {
-    authorId?: string
-    currentPage?: number
-    itemsPerPage?: number
-    maxCardsCount?: number
-    minCardsCount?: number
-    name?: string
-    orderBy?: Sort
+  authorId?: string
+  currentPage?: number
+  itemsPerPage?: number
+  maxCardsCount?: number
+  minCardsCount?: number
+  name?: string
+  orderBy?: Sort
 }
 
 export type DecksType = {
-    items?: ResponseDeckType[]
-    maxCardsCount?: number
-    pagination?: PaginationResponseType
+  items?: ResponseDeckType[]
+  maxCardsCount?: number
+  pagination?: PaginationResponseType
 }
 
 export type ResponseDeckTypeItemsAuthor = {
-    id: string
-    name: string
+  id: string
+  name: string
 }
 export type ResponseDeckType = {
-    author: ResponseDeckTypeItemsAuthor
-    id: string
-    userId: string
-    name: string
-    isPrivate: boolean
-    shots: number
-    cover: string
-    rating: number
-    created: string
-    updated: string
-    cardsCount: number
+  author: ResponseDeckTypeItemsAuthor
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  shots: number
+  cover: string
+  rating: number
+  created: string
+  updated: string
+  cardsCount: number
 }
 
 type DeleteDeckResponseType = {
-    id: string
-    userId: string
-    name: string
-    isPrivate: boolean
-    shots: number
-    cover: string
-    rating: number
-    created: string
-    updated: string
-    cardsCount: number
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  shots: number
+  cover: string
+  rating: number
+  created: string
+  updated: string
+  cardsCount: number
 }
 
 type UpdateDeckResponseType = {
-    author: UpdateDeckResponseTypeAuthor
-    id: string
-    userId: string
-    name: string
-    isPrivate: boolean
-    shots: number
-    cover: string
-    rating: number
-    created: string
-    updated: string
-    cardsCount: number
+  author: UpdateDeckResponseTypeAuthor
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  shots: number
+  cover: string
+  rating: number
+  created: string
+  updated: string
+  cardsCount: number
 }
 export type UpdateDeckResponseTypeAuthor = {
-    id: string
-    name: string
+  id: string
+  name: string
 }
 
 export type CreateDeckArgType = {
-    cover?: File
-    name: string
-    isPrivate?: boolean
+  cover?: File
+  name: string
+  isPrivate?: boolean
 }
 
 export const decksApi = baseApi.injectEndpoints({
-    endpoints: builder => {
-        return {
-            getDecks: builder.query<DecksType, GetDecksParamsType | void>({
-                providesTags: ['Decks'],
-                query: params => {
-                    return {
-                        params: params ?? undefined,
-                        url: `v1/decks`,
-                    }
-                },
-            }),
-            createDeck: builder.mutation<ResponseDeckType, CreateDeckArgType>({
-                invalidatesTags: ['Decks'],
-                query: params => {
-                    return {
-                        method: 'POST',
-                        url: `v1/decks`,
-                        body: params,
-                    }
-                },
-            }),
-            deleteDeck: builder.mutation<DeleteDeckResponseType, string>({
-                query: id => {
-                    return {
-                        method: 'DELETE',
-                        url: `v1/decks/${id}`,
-                    }
-                },
-                invalidatesTags: ['Decks'],
-            }),
-            updateDeck: builder.mutation<UpdateDeckResponseType, { form: FormData; id: string }>({
-                query: ({form, id}) => {
-                    return {
-                        method: 'PATCH',
-                        url: `v1/decks/${id}`,
-                        body: form,
-                    }
-                },
-                invalidatesTags: ['Decks'],
-            }),
-            learnRandomCard: builder.query<ResponseDeckType, string>({
-                query: id => {
-                    return {
-                        url: `v1/decks/${id}/learn`,
-                    }
-                },
-                providesTags: ['Decks', 'Cards'],
-            }),
-        }
-    },
+  endpoints: builder => {
+    return {
+      getDecks: builder.query<DecksType, GetDecksParamsType | void>({
+        providesTags: ['Decks'],
+        query: params => {
+          return {
+            params: params ?? undefined,
+            url: `v1/decks`,
+          }
+        },
+      }),
+      createDeck: builder.mutation<ResponseDeckType, CreateDeckArgType>({
+        invalidatesTags: ['Decks'],
+        query: params => {
+          return {
+            method: 'POST',
+            url: `v1/decks`,
+            body: params,
+          }
+        },
+      }),
+      deleteDeck: builder.mutation<DeleteDeckResponseType, string>({
+        query: id => {
+          return {
+            method: 'DELETE',
+            url: `v1/decks/${id}`,
+          }
+        },
+        invalidatesTags: ['Decks'],
+      }),
+      updateDeck: builder.mutation<UpdateDeckResponseType, { form: FormData; id: string }>({
+        query: ({ form, id }) => {
+          return {
+            method: 'PATCH',
+            url: `v1/decks/${id}`,
+            body: form,
+          }
+        },
+        invalidatesTags: ['Decks'],
+      }),
+      learnRandomCard: builder.query<CardsResponseType, string>({
+        query: id => {
+          return {
+            url: `v1/decks/${id}/learn`,
+          }
+        },
+        providesTags: ['Decks', 'Cards'],
+      }),
+    }
+  },
 })
 
 export const {
-    useLearnRandomCardQuery,
-    useCreateDeckMutation,
-    useGetDecksQuery,
-    useDeleteDeckMutation,
-    useUpdateDeckMutation,
+  useLearnRandomCardQuery,
+  useCreateDeckMutation,
+  useGetDecksQuery,
+  useDeleteDeckMutation,
+  useUpdateDeckMutation,
 } = decksApi
--- src/components/ui/decks/components/learnDeckModal/learnPack.tsx
+++ src/components/ui/decks/components/learnDeckModal/learnPack.tsx
@@ -1,14 +1,20 @@
-import {useParams} from "react-router-dom";
-import {useLearnRandomCardQuery} from "@/api/decks/decks.api.ts";
+import { useParams } from 'react-router-dom'
+import { useLearnRandomCardQuery } from '@/api/decks/decks.api.ts'
 
 export const LearnPack = () => {
-    const {id}= useParams()
-    const { data: learnCard } = useLearnRandomCardQuery(id)
-    return (
-        <div>
-            {learnCard?.name}
-            {/* Ваши компоненты для отображения данных */}
-        </div>
-    );
-};
+  const { id } = useParams()
+  const { data: learnCard } = useLearnRandomCardQuery(id!)
 
+  console.log(learnCard)
+  return (
+    <div style={{ color: 'red' }}>
+      <p> {learnCard?.question}</p>
+      <p> {learnCard?.answer}</p>
+
+      <img src={learnCard?.questionImg} alt="" />
+      <img src={learnCard?.answerImg} alt="" />
+
+      {/* Ваши компоненты для отображения данных */}
+    </div>
+  )
+}
