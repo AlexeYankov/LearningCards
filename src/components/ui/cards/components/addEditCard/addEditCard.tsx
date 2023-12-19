bc6c4058cd8627f2b480598f4e3f164f57d7a1f2
DK-AC <Denkacaj@gmail.com>
2023-12-19 21:04:57 +0300

    merge with ustin

--- src/components/ui/cards/components/addEditCard/addEditCard.tsx
+++ src/components/ui/cards/components/addEditCard/addEditCard.tsx
@@ -1,229 +1,230 @@
-import {ChangeEvent, FC, ReactNode, useState} from 'react'
-import {Button} from '../../../button/button.tsx'
-import {Select} from '@/components/ui/select'
-import {TextField} from '@/components/ui/textField'
-import {Modal, ModalTitle} from '@/components/ui/modal'
-import {useAppSelector} from '@/api/store.ts'
-import {CardsResponseType, useCreateCardMutation, useUpdateCardMutation,} from '@/api/common.api.ts'
-import {z} from 'zod'
-import {useForm} from 'react-hook-form'
+import { ChangeEvent, FC, ReactNode, useState } from 'react'
+import { Button } from '../../../button/button.tsx'
+import { Select } from '@/components/ui/select'
+import { TextField } from '@/components/ui/textField'
+import { Modal, ModalTitle } from '@/components/ui/modal'
+import { useAppSelector } from '@/api/store.ts'
+import {
+  CardsResponseType,
+  useCreateCardMutation,
+  useUpdateCardMutation,
+} from '@/api/common.api.ts'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
 import s from './addEditCard.module.scss'
-import {handleFileChange} from '@/components/ui/cards/components/addEditCard/handleFileChange.ts'
-import {EditIcon} from '@/asserts/icons/components/EditIcon.tsx'
-import {ImageSelector} from '@/components/ui/cards/components/addEditCard/ImageSelector.tsx'
-import {ErrorComponent} from "@/utils/toastify/Error.tsx";
+import { handleFileChange } from '@/components/ui/cards/components/addEditCard/handleFileChange.ts'
+import { EditIcon } from '@/asserts/icons/components/EditIcon.tsx'
+import { ImageSelector } from '@/components/ui/cards/components/addEditCard/ImageSelector.tsx'
+import { ErrorComponent } from '@/utils/toastify/Error.tsx'
 
 type Props = {
-    id?: string
-    editIcon?: ReactNode
-    card?: CardsResponseType
+  id?: string
+  editIcon?: ReactNode
+  card?: CardsResponseType
 }
 
 const schema = z.object({
-    questionImg: z.array(z.instanceof(File)),
-    answerImg: z.array(z.instanceof(File)),
-    answer: z.string().min(3),
-    question: z.string().min(3),
+  questionImg: z.array(z.instanceof(File)),
+  answerImg: z.array(z.instanceof(File)),
+  answer: z.string().min(3),
+  question: z.string().min(3),
 })
 
 type Form = z.infer<typeof schema>
 
-export const AddEditCard: FC<Props> = ({id, editIcon, card}) => {
-    const [selectedQuestionImage, setSelectedQuestionImage] = useState('')
-    const [selectedAnswerImage, setSelectedAnswerImage] = useState('')
-    const [open, setOpen] = useState(false)
-    const [selectedEditQuestionImage, setSelectedEditQuestionImage] = useState(card?.questionImg)
-    const [selectedEditAnswerImage, setSelectedEditAnswerImage] = useState(card?.answerImg)
+export const AddEditCard: FC<Props> = ({ id, editIcon, card }) => {
+  const [selectedQuestionImage, setSelectedQuestionImage] = useState('')
+  const [selectedAnswerImage, setSelectedAnswerImage] = useState('')
+  const [open, setOpen] = useState(false)
+  const [selectedEditQuestionImage, setSelectedEditQuestionImage] = useState(card?.questionImg)
+  const [selectedEditAnswerImage, setSelectedEditAnswerImage] = useState(card?.answerImg)
 
-    const valueSelect = useAppSelector(state => state.cards.valueSelect)
-    const {
-        register,
-        setValue,
-        setError,
-        handleSubmit,
-        reset,
-        formState: {errors},
-    } = useForm<Form>()
+  const valueSelect = useAppSelector(state => state.cards.valueSelect)
+  const {
+    register,
+    setValue,
+    setError,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Form>()
 
-    const [createCard, {error, isError}] = useCreateCardMutation()
-    const [updateCard] = useUpdateCardMutation()
+  const [createCard, { error, isError }] = useCreateCardMutation()
+  const [updateCard] = useUpdateCardMutation()
 
-    const deleteCardQuestion = () => {
-        setSelectedEditQuestionImage('')
-    }
-    const deleteCardAnswer = () => {
-        setSelectedEditAnswerImage('')
-    }
-    const handleModalToggle = () => {
-        setOpen(prevState => !prevState)
-        reset()
-        setSelectedQuestionImage('')
-        setSelectedAnswerImage('')
-    }
-    const handleQuestionFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-        handleFileChange({
-            setSelectedImage: editIcon ? setSelectedEditQuestionImage : setSelectedQuestionImage,
-            setValue,
-            event,
-            inputName: 'questionImg',
-        })
-    }
-
-    const handleAnswerFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-        handleFileChange({
-            setSelectedImage: editIcon ? setSelectedEditAnswerImage : setSelectedAnswerImage,
-            setValue,
-            event,
-            inputName: 'answerImg',
-        })
-    }
-
-    const onSubmit = handleSubmit(data => {
-        const form = new FormData()
-        form.append('question', data.question)
-        form.append('answer', data.answer)
-
-
-        if (data.questionImg && data.questionImg[0]) {
-            form.append('questionImg', data.questionImg[0])
-        }
-
-        if (data.answerImg && data.answerImg[0]) {
-            form.append('answerImg', data.answerImg[0])
-        }
-        if (editIcon) {
-            if (!selectedEditAnswerImage) {
-                form.append('answerImg', selectedEditAnswerImage!)
-            }
-            if (!selectedEditQuestionImage) {
-                form.append('questionImg', selectedEditQuestionImage!)
-
-            }
-        }
-        if (
-            (data.answer.trim() !== '' && data.answer.length >= 3) ||
-            (data.question.trim() !== '' && data.question.length >= 3)
-        ) {
-            if (editIcon) {
-                updateCard({
-                    id: card?.id!,
-                    data: form,
-                })
-            } else {
-                createCard({
-                    id: id!,
-                    data: form
-                })
-            }
-            handleModalToggle()
-        } else {
-            setError('question', {message: 'String must contain at least 3 character(s)'})
-            setError('answer', {message: 'String must contain at least 3 character(s)'})
-        }
+  const deleteCardQuestion = () => {
+    setSelectedEditQuestionImage('')
+  }
+  const deleteCardAnswer = () => {
+    setSelectedEditAnswerImage('')
+  }
+  const handleModalToggle = () => {
+    setOpen(prevState => !prevState)
+    reset()
+    setSelectedQuestionImage('')
+    setSelectedAnswerImage('')
+  }
+  const handleQuestionFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    handleFileChange({
+      setSelectedImage: editIcon ? setSelectedEditQuestionImage : setSelectedQuestionImage,
+      setValue,
+      event,
+      inputName: 'questionImg',
     })
+  }
 
-    return (
-        <>
-            <ErrorComponent error={error} isError={isError}/>
-            <Modal
-                onOpenChange={handleModalToggle}
-                open={open}
-                className={s.contentComponents}
-                triggerName={
-                    editIcon ? (
-                        <button>
-                            <EditIcon/>
-                        </button>
-                    ) : (
-                        <Button>Add new card</Button>
-                    )
-                }
-            >
+  const handleAnswerFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    handleFileChange({
+      setSelectedImage: editIcon ? setSelectedEditAnswerImage : setSelectedAnswerImage,
+      setValue,
+      event,
+      inputName: 'answerImg',
+    })
+  }
 
-                {editIcon ? <ModalTitle title={'Edit Card'}/> : <ModalTitle title={'Add New Card'}/>}
+  const onSubmit = handleSubmit(data => {
+    const form = new FormData()
+    form.append('question', data.question)
+    form.append('answer', data.answer)
 
-                <div className={s.contentBox}>
-                    <div className={s.select}>
-                        <Select
-                            label={'Choose question format'}
-                            options={['Text', 'Picture']}
-                            reversed
-                            selectId={'Select-box'}
-                        />
-                    </div>
-                    <form onSubmit={onSubmit}>
-                        {valueSelect === 'Picture' ? (
-                            <>
-                                <>
-                                    <TextField
-                                        inputId={'Input1'}
-                                        label={'Question'}
-                                        placeholder={'Question'}
-                                        errorMessage={errors.question?.message}
-                                        {...register('question', {value: card?.question})}
-                                    />
-                                    <ImageSelector
-                                        selectedImage={editIcon ? selectedEditQuestionImage : selectedQuestionImage}
-                                        deleteLabel={editIcon ? 'Delete Image' : ''}
-                                        onChange={handleQuestionFileChange}
-                                        changeLabel={'Change Question Image'}
-                                        inputId={'question-img-input'}
-                                        onImageDelete={deleteCardQuestion}
-                                    />
-                                </>
-                            </>
-                        ) : (
-                            <TextField
-                                inputId={'Input1'}
-                                label={'Question'}
-                                placeholder={'Question'}
-                                errorMessage={errors.question?.message}
-                                {...register('question', {value: card?.question})}
-                            />
-                        )}
-                        {valueSelect === 'Picture' ? (
-                            <>
-                                <TextField
-                                    inputId={'Input2'}
-                                    label={'Answer'}
-                                    placeholder={'Answer'}
-                                    errorMessage={errors.answer?.message}
-                                    {...register('answer', {value: card?.answer})}
-                                />
-                                <ImageSelector
-                                    selectedImage={editIcon ? selectedEditAnswerImage : selectedAnswerImage}
-                                    deleteLabel={editIcon ? 'Delete Image' : ''}
-                                    onChange={handleAnswerFileChange}
-                                    changeLabel={'Change Question Image'}
-                                    inputId={'answer-img-input'}
-                                    onImageDelete={deleteCardAnswer}
-                                />
-                            </>
-                        ) : (
-                            <TextField
-                                inputId={'Input2'}
-                                label={'Answer'}
-                                placeholder={'Answer'}
-                                errorMessage={errors.answer?.message}
-                                {...register('answer', {value: card?.answer})}
-                            />
-                        )}
-                        <div className={`${s.contentBtn} ${s.contentBtns}`}>
-                            <Button classNameBtnBox={s.btnBox} onClick={handleModalToggle} variant={'secondary'}>
-                                Close
-                            </Button>
-                            <Button
-                                classNameBtnBox={s.btnBox}
-                                variant={'primary'}
-                                disabled={!!errors.answer?.message}
-                                type={'submit'}
-                            >
-                                {editIcon ? 'Save Changes' : 'Add new card'}
-                            </Button>
-                        </div>
-                    </form>
-                </div>
-            </Modal>
-        </>
-    )
+    if (data.questionImg && data.questionImg[0]) {
+      form.append('questionImg', data.questionImg[0])
+    }
+
+    if (data.answerImg && data.answerImg[0]) {
+      form.append('answerImg', data.answerImg[0])
+    }
+    if (editIcon) {
+      if (!selectedEditAnswerImage) {
+        form.append('answerImg', selectedEditAnswerImage!)
+      }
+      if (!selectedEditQuestionImage) {
+        form.append('questionImg', selectedEditQuestionImage!)
+      }
+    }
+    if (
+      (data.answer.trim() !== '' && data.answer.length >= 3) ||
+      (data.question.trim() !== '' && data.question.length >= 3)
+    ) {
+      if (editIcon) {
+        updateCard({
+          id: card?.id!,
+          data: form,
+        })
+      } else {
+        createCard({
+          id: id!,
+          data: form,
+        })
+      }
+      handleModalToggle()
+    } else {
+      setError('question', { message: 'String must contain at least 3 character(s)' })
+      setError('answer', { message: 'String must contain at least 3 character(s)' })
+    }
+  })
+
+  return (
+    <>
+      <ErrorComponent error={error} isError={isError} />
+      <Modal
+        onOpenChange={handleModalToggle}
+        open={open}
+        className={s.contentComponents}
+        triggerName={
+          editIcon ? (
+            <button>
+              <EditIcon />
+            </button>
+          ) : (
+            <Button>Add new card</Button>
+          )
+        }
+      >
+        {editIcon ? <ModalTitle title={'Edit Card'} /> : <ModalTitle title={'Add New Card'} />}
+
+        <div className={s.contentBox}>
+          <div className={s.select}>
+            <Select
+              label={'Choose question format'}
+              options={['Text', 'Picture']}
+              reversed
+              selectId={'Select-box'}
+            />
+          </div>
+          <form onSubmit={onSubmit}>
+            {valueSelect === 'Picture' ? (
+              <>
+                <>
+                  <TextField
+                    inputId={'Input1'}
+                    label={'Question'}
+                    placeholder={'Question'}
+                    errorMessage={errors.question?.message}
+                    {...register('question', { value: card?.question })}
+                  />
+                  <ImageSelector
+                    selectedImage={editIcon ? selectedEditQuestionImage : selectedQuestionImage}
+                    deleteLabel={editIcon ? 'Delete Image' : ''}
+                    onChange={handleQuestionFileChange}
+                    changeLabel={'Change Question Image'}
+                    inputId={'question-img-input'}
+                    onImageDelete={deleteCardQuestion}
+                  />
+                </>
+              </>
+            ) : (
+              <TextField
+                inputId={'Input1'}
+                label={'Question'}
+                placeholder={'Question'}
+                errorMessage={errors.question?.message}
+                {...register('question', { value: card?.question })}
+              />
+            )}
+            {valueSelect === 'Picture' ? (
+              <>
+                <TextField
+                  inputId={'Input2'}
+                  label={'Answer'}
+                  placeholder={'Answer'}
+                  errorMessage={errors.answer?.message}
+                  {...register('answer', { value: card?.answer })}
+                />
+                <ImageSelector
+                  selectedImage={editIcon ? selectedEditAnswerImage : selectedAnswerImage}
+                  deleteLabel={editIcon ? 'Delete Image' : ''}
+                  onChange={handleAnswerFileChange}
+                  changeLabel={'Change Question Image'}
+                  inputId={'answer-img-input'}
+                  onImageDelete={deleteCardAnswer}
+                />
+              </>
+            ) : (
+              <TextField
+                inputId={'Input2'}
+                label={'Answer'}
+                placeholder={'Answer'}
+                errorMessage={errors.answer?.message}
+                {...register('answer', { value: card?.answer })}
+              />
+            )}
+            <div className={`${s.contentBtn} ${s.contentBtns}`}>
+              <Button classNameBtnBox={s.btnBox} onClick={handleModalToggle} variant={'secondary'}>
+                Close
+              </Button>
+              <Button
+                classNameBtnBox={s.btnBox}
+                variant={'primary'}
+                disabled={!!errors.answer?.message}
+                type={'submit'}
+              >
+                {editIcon ? 'Save Changes' : 'Add new card'}
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+    </>
+  )
 }
