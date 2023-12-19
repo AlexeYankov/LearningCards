14a833c2f3f4ea18dfe0745dbfd22cd1e7ec4f05
DK-AC <Denkacaj@gmail.com>
2023-12-19 19:50:49 +0300

    fix: styles cards

--- src/components/ui/decks/decksPage.tsx
+++ src/components/ui/decks/decksPage.tsx
@@ -1,4 +1,4 @@
-import {useGetDecksQuery} from '@/api/decks/decks.api'
+import { useGetDecksQuery } from '@/api/decks/decks.api'
 import s from './decksPage.module.scss'
 
 import { Pagination } from '../pagination'
@@ -12,7 +12,7 @@ import { DecksPageName } from './components/decksPageName'
 import { DecksHead } from './components/decksHead'
 import { DecksBody } from './components/decksBody'
 import { useSearchParams } from 'react-router-dom'
-import {ErrorComponent} from "@/utils/toastify/Error.tsx"
+import { ErrorComponent } from '@/utils/toastify/Error.tsx'
 
 export const DecksPage = () => {
   const dispatch = useAppDispatch()
@@ -26,7 +26,11 @@ export const DecksPage = () => {
   const authorId = useAppSelector(state => state.decks.authorId)
   const name = useAppSelector(state => state.decks.name)
 
-  const { data: decks,isError,error } = useGetDecksQuery({
+  const {
+    data: decks,
+    isError,
+    error,
+  } = useGetDecksQuery({
     currentPage,
     itemsPerPage,
     maxCardsCount,
@@ -47,34 +51,33 @@ export const DecksPage = () => {
 
     if (totalItems) {
       dispatch(changeItemsPerPage({ itemsPerPage: 10 }))
-      // Обновление значения currentPage в URL при каждом изменении
       setSearchParams({ ...searchParams, page: String(currentPage) })
     }
   }, [currentPage, decks?.pagination?.totalItems, dispatch, setSearchParams, searchParams])
   return (
     <div className={s.container}>
-      <ErrorComponent error={error} isError={isError}/>
+      <ErrorComponent error={error} isError={isError} />
       <DecksPageName />
       <DecksPageBar />
       {isDecksEmpty ? (
-          <div className={s.decksEmpty}>No content with these terms...</div>
-      ):(
-          <>
-            <Root className={s.container__common}>
-              <DecksHead />
-              <DecksBody decks={decks} />
-            </Root>
-            {isShowPagination && decks?.pagination?.totalItems! > 10 && (
-                <Pagination
-                    reversed
-                    arrowColor="white"
-                    arrowID="arrow-ios-back"
-                    reversedArrowID="arrow-ios-forward"
-                    totalPages={decks?.pagination.totalPages!}
-                    totalItems={decks?.pagination.totalItems!}
-                />
-            )}
-          </>
+        <div className={s.decksEmpty}>No content with these terms...</div>
+      ) : (
+        <>
+          <Root className={s.container__common}>
+            <DecksHead />
+            <DecksBody decks={decks} />
+          </Root>
+          {isShowPagination && decks?.pagination?.totalItems! > 10 && (
+            <Pagination
+              reversed
+              arrowColor="white"
+              arrowID="arrow-ios-back"
+              reversedArrowID="arrow-ios-forward"
+              totalPages={decks?.pagination.totalPages!}
+              totalItems={decks?.pagination.totalItems!}
+            />
+          )}
+        </>
       )}
     </div>
   )
