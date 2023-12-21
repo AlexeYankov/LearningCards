633cbac7cf8ecee42f9f4322f2e54b6fa334ef09
DK-AC <Denkacaj@gmail.com>
2023-12-21 20:05:45 +0300

    fix: search input

--- src/components/ui/cards/components/pageBar/pageBar.tsx
+++ src/components/ui/cards/components/pageBar/pageBar.tsx
@@ -1,23 +1,32 @@
-import {TextField} from '@/components/ui/textField'
+import { TextField } from '@/components/ui/textField'
 import f from '../../cardsPage.module.scss'
-import {ChangeEvent} from "react";
-
+import { ChangeEvent } from 'react'
+import { useAppDispatch } from '@/api/store.ts'
+import { changeCurrentPage } from '@/api/decks'
 
 type Props = {
-    onChange: (value: string) => void
-    value:string
+  onChange: (value: string) => void
+  value: string
 }
-export const PageBar = ({onChange,value}:Props) => {
-    const handleSearchValue = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange(e.currentTarget.value)
-    }
-    const handleClearSearchValueClick = () => {
-        onChange('')
-    }
-    return (
-        <div className={f.container__pageBar}>
-            <TextField  value={value} onChange={handleSearchValue} onClearClick={handleClearSearchValueClick}
-                       placeholder={'Input search'} search/>
-        </div>
-    )
+export const PageBar = ({ onChange, value }: Props) => {
+  const dispatch = useAppDispatch()
+
+  const handleSearchValue = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.currentTarget.value)
+    dispatch(changeCurrentPage({ currentPage: 1 }))
+  }
+  const handleClearSearchValueClick = () => {
+    onChange('')
+  }
+  return (
+    <div className={f.container__pageBar}>
+      <TextField
+        value={value}
+        onChange={handleSearchValue}
+        onClearClick={handleClearSearchValueClick}
+        placeholder={'Input search'}
+        search
+      />
+    </div>
+  )
 }
