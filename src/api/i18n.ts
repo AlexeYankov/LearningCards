c82443b2f73c9fa5a195781b0a1ce36e02acf98b
DK-AC <Denkacaj@gmail.com>
2023-12-27 10:05:47 +0300

    add: i18next to decksPageBar

--- src/api/i18n.ts
+++ src/api/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import enTranslation from './../../public/locales/en/enTranslation.json'
-import ruTranslation from './../../public/locales/ru/ruTranslation.json'
+import enTranslation from '../../public/locales/en/translation.json'
+import ruTranslation from '../../public/locales/ru/translation.json'
 
 i18n.use(initReactI18next).init({
   lng: 'en',
