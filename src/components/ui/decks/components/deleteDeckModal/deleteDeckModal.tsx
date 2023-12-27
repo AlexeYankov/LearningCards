31c25590d6305a2a32750d100bd5e956377f2260
DK-AC <Denkacaj@gmail.com>
2023-12-27 11:36:57 +0300

    add: i18next to deleteDeckModal

--- src/components/ui/decks/components/deleteDeckModal/deleteDeckModal.tsx
+++ src/components/ui/decks/components/deleteDeckModal/deleteDeckModal.tsx
@@ -5,72 +5,71 @@ import { DeleteIcon } from '@/asserts/icons'
 import { Typography } from '@/components/ui/typography'
 import s from '@/components/ui/decks/decksPage.module.scss'
 import { Button } from '@/components/ui/button'
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 type DeleteDeckModalProps = {
-    deck: ResponseDeckType
-    open?: boolean
-    setOpen?: (open: boolean) => void
-    hover?: boolean
+  deck: ResponseDeckType
+  open?: boolean
+  setOpen?: (open: boolean) => void
+  hover?: boolean
 }
 
-export const DeleteDeckModal = ({deck, setOpen, open, hover}: DeleteDeckModalProps) => {
+export const DeleteDeckModal = ({ deck, setOpen, open, hover }: DeleteDeckModalProps) => {
+  const { t } = useTranslation()
 
-    // const openD = useAppSelector(state => state.decks.open)
-    const [deleteDeck] = useDeleteDeckMutation()
-    const [openLocal, setOpenLocal] = useState(false)
+  const [deleteDeck] = useDeleteDeckMutation()
+  const [openLocal, setOpenLocal] = useState(false)
 
-    const handleCloseModal = () => {
-        // setOpen(prevState => !prevState)
-        setOpen ? setOpen(false)  :setOpenLocal(prevState => !prevState)
-    }
+  const handleCloseModal = () => {
+    setOpen ? setOpen(false) : setOpenLocal(prevState => !prevState)
+  }
 
-    const navigate = useNavigate()
-    const handleDeleteDeckClick = () => {
-        deleteDeck(deck.id!)
-        handleCloseModal()
-        navigate('/')
-    }
-    return (
-        <Modal
-            hover={hover}
-            open={open ? open : openLocal}
-            onOpenChange={setOpen ? setOpen : setOpenLocal}
-            triggerName={
-                <button>
-                    <DeleteIcon/>
-                </button>
-            }
+  const navigate = useNavigate()
+  const handleDeleteDeckClick = () => {
+    deleteDeck(deck.id!)
+    handleCloseModal()
+    navigate('/')
+  }
+  return (
+    <Modal
+      hover={hover}
+      open={open ? open : openLocal}
+      onOpenChange={setOpen ? setOpen : setOpenLocal}
+      triggerName={
+        <button>
+          <DeleteIcon />
+        </button>
+      }
+    >
+      <ModalTitle title={t('delete_deck')} />
+
+      <ModalDescription>
+        <Typography variant={'body1'} as={'p'}>
+          {t('delete_message')} <span className={s.boldText}>{deck.name}</span>?
+        </Typography>
+        <Typography variant={'body1'} as={'p'}>
+          {t('delete_message_description')}
+        </Typography>
+      </ModalDescription>
+      <div className={`${s.contentBtn} ${s.contentBtns}`}>
+        <Button
+          classNameBtnBox={s.btnBox}
+          onClick={handleCloseModal}
+          variant={'secondary'}
+          type={'button'}
         >
-            <ModalTitle title={'Delete Pack'}/>
-
-            <ModalDescription>
-                <Typography variant={'body1'} as={'p'}>
-                    Do you really want to remove <span className={s.boldText}>{deck.name}</span>?
-                </Typography>
-                <Typography variant={'body1'} as={'p'}>
-                    All cards will be deleted.
-                </Typography>
-            </ModalDescription>
-            <div className={`${s.contentBtn} ${s.contentBtns}`}>
-                <Button
-                    classNameBtnBox={s.btnBox}
-                    onClick={handleCloseModal}
-                    variant={'secondary'}
-                    type={'button'}
-                >
-                    Cancel
-                </Button>
-                <Button
-                    classNameBtnBox={s.btnBox}
-                    onClick={handleDeleteDeckClick}
-                    variant={'primary'}
-                    type={'submit'}
-                >
-
-                    Delete Pack
-                </Button>
-            </div>
-        </Modal>
-    )
+          {t('cancel')}
+        </Button>
+        <Button
+          classNameBtnBox={s.btnBox}
+          onClick={handleDeleteDeckClick}
+          variant={'primary'}
+          type={'submit'}
+        >
+          {t('delete_deck')}
+        </Button>
+      </div>
+    </Modal>
+  )
 }
