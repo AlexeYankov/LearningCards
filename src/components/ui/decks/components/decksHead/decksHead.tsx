4634e121269e9fc74e63d1d7c38037a5080e4024
DK-AC <Denkacaj@gmail.com>
2023-12-27 10:17:23 +0300

    add: i18next to decksHead

--- src/components/ui/decks/components/decksHead/decksHead.tsx
+++ src/components/ui/decks/components/decksHead/decksHead.tsx
@@ -1,13 +1,44 @@
 import s from '@/components/ui/decks/decksPage.module.scss'
 import { Head, HeadCell, Row } from '@it-incubator/ui-kit'
-import { columns } from '@/components/ui/decks'
 import { changeOrderBy } from '@/api/decks'
 import { useAppDispatch, useAppSelector } from '@/api/store'
+import { useTranslation } from 'react-i18next'
+import { Column } from '@/components/ui/decks'
 
 export const DecksHead = () => {
   const dispatch = useAppDispatch()
 
   const sort = useAppSelector(state => state.decks.sort)
+
+  const { t } = useTranslation()
+
+  const columns: Column[] = [
+    {
+      key: 'name',
+      title: t('name'),
+      sortable: true,
+    },
+    {
+      key: 'cardsCount',
+      title: t('cards'),
+      sortable: true,
+    },
+    {
+      key: 'updated',
+      title: t('last_updated'),
+      sortable: true,
+    },
+    {
+      key: 'author.name',
+      title: t('created_by'),
+      sortable: true,
+    },
+    {
+      key: 'actions',
+      title: '',
+      sortable: false,
+    },
+  ]
 
   const handleSort = (key: string) => {
     if (key !== 'actions') {
--- src/components/ui/decks/decksData.ts
+++ src/components/ui/decks/decksData.ts
@@ -10,31 +10,3 @@ export type Sort = {
   key: string
   direction: SortDescriptionType
 } | null
-
-export const columns: Column[] = [
-  {
-    key: 'name',
-    title: 'Name',
-    sortable: true,
-  },
-  {
-    key: 'cardsCount',
-    title: 'Cards',
-    sortable: true,
-  },
-  {
-    key: 'updated',
-    title: 'Last Updated',
-    sortable: true,
-  },
-  {
-    key: 'author.name',
-    title: 'Created by',
-    sortable: true,
-  },
-  {
-    key: 'actions',
-    title: '',
-    sortable: false,
-  },
-]
