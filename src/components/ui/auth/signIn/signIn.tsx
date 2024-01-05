9b79f34f49af225276b69d7fd67c27bdfc78f814
DK-AC <Denkacaj@gmail.com>
2024-01-05 18:08:55 +0300

    refactor: sign in and check-box

--- src/components/ui/auth/signIn/signIn.tsx
+++ src/components/ui/auth/signIn/signIn.tsx
@@ -1,41 +1,47 @@
-import s from './signIn.module.scss'
-import { Typography } from '@/components/ui/typography'
-import { z } from 'zod'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { useLoginMutation } from '@/api/auth'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
-import { Link, useNavigate } from 'react-router-dom'
-import { useLoginMutation } from '@/api/auth'
+import { CheckBox } from '@/components/ui/checkbox'
 import { Progress } from '@/components/ui/loader'
 import { TextField } from '@/components/ui/textField'
-import { CheckBox } from '@/components/ui/checkbox'
+import { Typography } from '@/components/ui/typography'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import s from './signIn.module.scss'
 
 type FormValues = z.infer<typeof loginSchema>
 
 const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(3),
-  rememberMe: z.boolean().default(false),
 })
+
 export const SignIn = () => {
   const navigate = useNavigate()
 
   const {
+    formState: { errors },
     handleSubmit,
     register,
-    formState: { errors },
   } = useForm<FormValues>({
-    resolver: zodResolver(loginSchema),
-    mode: 'onSubmit',
     defaultValues: {
       email: '',
       password: '',
-      rememberMe: false,
     },
+    mode: 'onSubmit',
+    resolver: zodResolver(loginSchema),
   })
 
   const [login, { isLoading }] = useLoginMutation()
+  const [isPrivate, setIsPrivate] = useState(false)
+  const handeCheckedChange = () => {
+    setIsPrivate(prevState => !prevState)
+  }
 
   const onSubmit = (data: FormValues) => {
     login(data).then(() => {
@@ -43,81 +49,70 @@ export const SignIn = () => {
     })
   }
 
-  if (isLoading) {
-    return <Progress />
-  }
-
   return (
-    <Card className={s.signIn}>
-      <Typography className={s.label} children={'Sign In'} variant={'large'} />
-      <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          placeholder={'Email'}
-          type={'text'}
-          label={'Email'}
-          inputId={'inputEmailSignUp'}
-          errorMessage={errors.email && errors.email?.message}
-          {...register('email')}
-        />
-        <TextField
-          placeholder={'Password'}
-          type={'password'}
-          label={'Password'}
-          password
-          inputId={'inputPasswordSignUp'}
-          errorMessage={errors.password && errors.password?.message}
-          {...register('password')}
-        />
-        <div className={s.checkboxContainer}>
-          <CheckBox
-            className={s.checkbox}
-            width={'24'}
-            height={'24'}
-            IconID={'checkbox-unselected'}
-            SelectedIconID={'checkbox-selected'}
-            label={'Remember me'}
-            checkboxId={'ControlledCheckboxSignIn'}
-            {...register('rememberMe')}
+    <>
+      {isLoading && Progress}
+      <Card className={s.signIn}>
+        <Typography className={s.label} variant={'large'}>
+          Sign In
+        </Typography>
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            errorMessage={errors.email && errors.email?.message}
+            inputId={'inputEmailSignUp'}
+            label={'Email'}
+            placeholder={'Email'}
+            type={'text'}
+            {...register('email')}
           />
-        </div>
-        <div className={s.linkContainer}>
-          <Typography
-            className={s.forgotPassLink}
-            children={'Forgot Password?'}
-            variant={'body2'}
-            as={Link}
-            to={'/forgotYourPassword'}
+          <TextField
+            errorMessage={errors.password && errors.password?.message}
+            inputId={'inputPasswordSignUp'}
+            label={'Password'}
+            password
+            placeholder={'Password'}
+            type={'password'}
+            {...register('password')}
           />
-        </div>
-        <Button
-          className={s.button}
-          type={'submit'}
-          variant={'primary'}
-          fullWidth
-          children={<Typography children={'Sign In'} variant={'subtitle2'} as={'p'} />}
-        />
-      </form>
+          <div className={s.checkboxContainer}>
+            <CheckBox
+              IconID={'checkbox-unselected'}
+              SelectedIconID={'checkbox-selected'}
+              checkboxId={'ControlledCheckboxSignIn'}
+              checked={isPrivate}
+              className={s.checkbox}
+              height={'24'}
+              label={'Remember me'}
+              onChange={handeCheckedChange}
+              width={'24'}
+            />
+          </div>
+          <div className={s.linkContainer}>
+            <Typography
+              as={Link}
+              className={s.forgotPassLink}
+              to={'/forgotYourPassword'}
+              variant={'body2'}
+            >
+              Forgot Password?
+            </Typography>
+          </div>
+          <Button className={s.button} fullWidth type={'submit'} variant={'primary'}>
+            <Typography as={'p'} variant={'subtitle2'}>
+              Sign In
+            </Typography>
+          </Button>
+        </form>
 
-      <Typography
-        className={s.linkDontHaveAccount}
-        children={"Don't have an account?"}
-        variant={'body2'}
-        as={'p'}
-      />
-      <Button
-        className={s.btnSignUp}
-        type={'button'}
-        variant={'link'}
-        children={
-          <Typography
-            className={s.linkSignUp}
-            children={'Sign Up'}
-            variant={'subtitle2'}
-            to={'/signUp'}
-            as={Link}
-          />
-        }
-      />
-    </Card>
+        <Typography as={'p'} className={s.linkDontHaveAccount} variant={'body2'}>
+          Don&apos;t have an account?
+        </Typography>
+        <Button className={s.btnSignUp} type={'button'} variant={'link'}>
+          <Typography as={Link} className={s.linkSignUp} to={'/signUp'} variant={'subtitle2'}>
+            Sign Up
+          </Typography>
+        </Button>
+      </Card>
+    </>
   )
 }
--- src/components/ui/checkbox/check-box.tsx
+++ src/components/ui/checkbox/check-box.tsx
@@ -1,28 +1,40 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 
+import sprite from '@/asserts/sprite.svg'
+
 import s from './check-box.module.scss'
 
-import sprite from '@/asserts/sprite.svg'
 import { Label } from '../label'
 
+type Props<T extends ElementType = 'input'> = {
+  IconID?: string
+  SelectedIconID?: string
+  checkboxId?: string
+  className?: string
+  fullWidth?: boolean
+  height?: string
+  label?: string
+  width?: string
+} & ComponentPropsWithoutRef<T>
+
 export const CheckBox = <T extends ElementType = 'input'>(
-  props: CheckBoxProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof CheckBoxProps<T>>
+  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>
 ) => {
   const {
     IconID,
     SelectedIconID,
     as: Component = 'input',
+    checkboxId,
+    checked,
     className,
     disabled = false,
     fullWidth,
     height = '100%',
     label,
+    onChange,
     theme = 'white',
     viewBox = '0 0 24 24',
     width = '100%',
-    checkboxId,
-    onChange,
-    checked,
     ...rest
   } = props
 
@@ -55,14 +67,3 @@ export const CheckBox = <T extends ElementType = 'input'>(
     </div>
   )
 }
-
-export type CheckBoxProps<T extends ElementType = 'input'> = {
-  checkboxId?: string
-  IconID?: string
-  SelectedIconID?: string
-  className?: string
-  fullWidth?: boolean
-  height?: string
-  label?: string
-  width?: string
-} & ComponentPropsWithoutRef<T>
