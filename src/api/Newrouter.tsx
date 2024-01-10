c1838a570e4b7121a7d9e4a4db6626894713d7e6
Ustin4 <Mortyvp@yandex.by>
2024-01-10 17:57:47 +0300

    urlLang

---
+++ src/api/Newrouter.tsx
@@ -0,0 +1,50 @@
+// import { CheckEmail } from '@/components/ui/auth/checkEmail'
+// import { CreateNewPassword } from '@/components/ui/auth/createNewPassword'
+// import { ForgotYourPassword } from '@/components/ui/auth/forgotYourPassword'
+// import { SignIn } from '@/components/ui/auth/signIn'
+// import { SignUp } from '@/components/ui/auth/signUp'
+// import { CardsPage, LearnDeck } from '@/components/ui/cards'
+// import { DecksPage } from '@/components/ui/decks'
+// import { EditProfile } from '@/components/ui/editProfile'
+//
+// export const authRoutes = [
+//   {
+//     element: <SignIn />,
+//     path: '/login',
+//   },
+//   {
+//     element: <SignUp />,
+//     path: '/signUp',
+//   },
+//   {
+//     element: <CreateNewPassword />,
+//     path: '/createNewPassword/*',
+//   },
+//   {
+//     element: <CheckEmail />,
+//     path: '/:email/checkEmail',
+//   },
+//   {
+//     element: <ForgotYourPassword />,
+//     path: '/forgotYourPassword',
+//   },
+// ]
+//
+// export const publicRoutes = [
+//   {
+//     element: <LearnDeck />,
+//     path: '/:id/learn',
+//   },
+//   {
+//     element: <DecksPage />,
+//     path: '/',
+//   },
+//   {
+//     element: <CardsPage />,
+//     path: '/:id',
+//   },
+//   {
+//     element: <EditProfile />,
+//     path: '/profile',
+//   },
+// ]
